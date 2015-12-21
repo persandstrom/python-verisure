48e81389f9b5902cbe3f4a0b682a847a7dccab64
lindehoff <jacob@cobmedia.se>
2015-12-21 13:20:35 +0100

    Fixes wait_while_pending sometimes returns -1 even though the command worked
    In some cases wait_while_pending return -1, I've added a `print response`for debuging purpes in example below
    Original code outputs:
    ```myPages.alarm.wait_while_pending()
    {'hasResult': False, 'hasPending': True}
    {'hasResult': False, 'hasPending': True}
    {'hasResult': True, 'hasPending': True}
    -1 ```
    If I remove `or response['hasResult']` I get:
    ```myPages.alarm.wait_while_pending()
    {'hasResult': False, 'hasPending': True}
    {'hasResult': False, 'hasPending': True}
    {'hasResult': False, 'hasPending': True}
    {'hasResult': True, 'hasPending': True}
    {'hasResult': True, 'hasPending': True}
    {'hasResult': True, 'hasPending': False}
    5 ```
    So it seems to work better.

--- verisure/devices/alarm.py
+++ verisure/devices/alarm.py
@@ -52,7 +52,7 @@ class Alarm(object):
         for counter in range(max_request_count):
             data = {'counter': counter}
             response = self._session.json_to_dict(self._session.post(CHECK_STATE, data))
-            if 'hasResult' not in response or not response['hasResult']:
+            if 'hasResult' not in response:
                 break
             if 'hasPending' in response and not response['hasPending']:
                 return counter
