56fb5129f459d775585cc42f75919cec2c940598
Per Sandström <per.j.sandstrom@gmail.com>
2016-01-07 19:12:40 +0100

    use json and beautifulsoap

--- verisure/session.py
+++ verisure/session.py
@@ -4,14 +4,9 @@ import re
 
 import requests
 
-# this import is depending on python version
-try:
-    import HTMLParser
-    UNESCAPE = HTMLParser.HTMLParser().unescape
-except ImportError:
-    import html
-    UNESCAPE = html.unescape
+import json
 
+from bs4 import BeautifulSoup
 
 DOMAIN = 'https://mypages.verisure.com'
 URL_LOGIN = DOMAIN + '/j_spring_security_check?locale=en_GB'
@@ -22,7 +17,7 @@ CSRF_REGEX = re.compile(
     r'(?P<csrf>([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}))' +
     r'" /\>')
 
-TITLE_REGEX = re.compile(r'\<title\>(?P<title>(.*))\</title\>')
+# TITLE_REGEX = re.compile(r'\<title\>(?P<title>(.*))\</title\>')
 
 
 class Error(Exception):
@@ -142,46 +137,41 @@ class Session(object):
         if not self._session:
             raise Error('Session not started')
 
-    # pylint: disable=W0612,W0123
-    @staticmethod
-    def json_to_dict(json):
+    def json_to_dict(self, doc):
         ''' transform json with unicode characters to dict '''
-
-        true, false, null = True, False, None  # noqa
         try:
-            orgJson = json.encode('utf-8')
-            return eval(UNESCAPE(json))
-        except Exception as e:
-            match = TITLE_REGEX.search(orgJson)
-            if match:
-                if match.group('title') == u'My Pages is temporarily unavailable -  Verisure':
-                    raise TemporarilyUnavailableError('Temporarily unavailable')
-                elif match.group('title') == u'My Pages - Maintenance -  Verisure':
-                    raise MaintenanceError('Maintenance')
-                elif (match.group('title') == u'Choose country - My Pages - Verisure' or
-                      match.group('title') == u'Log in - My Pages - Verisure'):
-                    raise LoggedOutError('Not logged in')
-                else:
-                    raise ResponseError(match.group('title'))
-            else:
-                raise ResponseError('Unable to convert to JSON, Error: {0} - Data: {1}'.format(e, orgJson))
+            return json.loads(doc)
+        except ValueError as e:
+            self.raise_response_error(
+                doc,
+                ResponseError(
+                    'Unable to convert to JSON, '
+                    'Error: {0} - Data: {1}'.format(e, doc)))
+
+    def validate_response(self, response):
+        """ Verify that response is OK """
+        if response.status_code == 200:
+            return
+        self.raise_response_error(
+            response.text,
+            ResponseError(
+                'Unable to validate response form My Pages'
+                ', status code: {0} - Data: {1}'.format(
+                    response.status_code,
+                    response.text.encode('utf-8'))))
 
     @staticmethod
-    def validate_response(response):
-        """ Verify that response is OK """
-        if response.status_code != 200:
-            match = TITLE_REGEX.search(response.text)
-            if match:
-                if match.group('title') == u'My Pages is temporarily unavailable -  Verisure':
-                    raise TemporarilyUnavailableError('Temporarily unavailable')
-                elif match.group('title') == u'My Pages - Maintenance -  Verisure':
-                    raise MaintenanceError('Maintenance')
-                elif (match.group('title') == u'Choose country - My Pages - Verisure' or
-                      match.group('title') == u'Log in - My Pages - Verisure'):
-                    raise LoggedOutError('Not logged in')
-                else:
-                    raise ResponseError(match.group('title'))
-            raise ResponseError(
-                'Unable to validate response form My Pages, status code: {0} - Data: {1}'.format(
-                    response.status_code,
-                    response.text.encode('utf-8')))
+    def raise_response_error(doc, default_error):
+        """ Create correct error type from response """
+        soup = BeautifulSoup(doc, 'html.parser')
+        if not soup.tile:
+            raise default_error
+        if soup.title == 'My Pages is temporarily unavailable -  Verisure':
+            raise TemporarilyUnavailableError('Temporarily unavailable')
+        if soup.title == 'My Pages - Maintenance -  Verisure':
+            raise MaintenanceError('Maintenance')
+        if soup.title == 'Choose country - My Pages - Verisure':
+            raise LoggedOutError('Not logged in')
+        if soup.title == 'Log in - My Pages - Verisure':
+            raise LoggedOutError('Not logged in')
+        raise ResponseError(soup.title)
