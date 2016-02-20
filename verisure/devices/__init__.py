c432145bb2c99c5154a53865d33608f3f88cf1df
Per Sandström <per.j.sandstrom@gmail.com>
2016-02-20 22:30:38 +0100

    change from heatpump to temperaturecontrol

--- verisure/devices/__init__.py
+++ verisure/devices/__init__.py
@@ -4,20 +4,20 @@ __all__ = [
     'Alarm',
     'Climate',
     'Ethernet',
-    'Heatpump',
     'Lock',
     'Mousedetection',
     'Smartcam',
     'Smartplug',
+    'Temperaturecontrol',
     'Vacationmode',
 ]
 
 from .alarm import Alarm
 from .climate import Climate
 from .ethernet import Ethernet
-from .heatpump import Heatpump
 from .lock import Lock
 from .mousedetection import Mousedetection
 from .smartcam import Smartcam
 from .smartplug import Smartplug
+from .temperaturecontrol import Temperaturecontrol
 from .vacationmode import Vacationmode
