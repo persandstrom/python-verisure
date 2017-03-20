f9d792f58d1ddcabce1c31164a6df5dcdc134100
daweedm <m@daweed.be>
2017-03-20 19:26:12 +0100

    automatic logout on object instance destruction

--- verisure/session.py
+++ verisure/session.py
@@ -506,3 +506,5 @@ class Session(object):
         except requests.exceptions.RequestException as ex:
             raise RequestError(ex)
         _validate_response(response)
+    def __del__(self):
+        self.logout()
